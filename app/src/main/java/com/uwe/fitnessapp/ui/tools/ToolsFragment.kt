300b37effe0d3a0559efb800f9c767372b078547
baktygaliyevv <116110183+baktygaliyevv@users.noreply.github.com>
2024-10-17 15:07:31 +0100

    buttons design is done

--- app/src/main/java/com/uwe/fitnessapp/ui/tools/ToolsFragment.kt
+++ app/src/main/java/com/uwe/fitnessapp/ui/tools/ToolsFragment.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.uwe.fitnessapp.R
 import com.uwe.fitnessapp.databinding.FragmentToolsBinding
@@ -17,34 +17,49 @@ class ToolsFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val toolsViewModel =
-            ViewModelProvider(this).get(ToolsViewModel::class.java)
-
         _binding = FragmentToolsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textTools
-        toolsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        addCard(
+            title = "BMI Calculator",
+            description = "BMI Calculator will calculate your body mass index and give some recommendations to your diet",
+            iconRes = R.drawable.ic_bmi_metabolism_black_24dp,
+            onClickAction = { findNavController().navigate(R.id.navigation_bmi) }
+        )
 
-        binding.btnBmi.setOnClickListener {
-            findNavController().navigate(R.id.navigation_bmi)
-        }
+        addCard(
+            title = "Calories Calculator",
+            description = "Calculate your daily calorie intake depends on your goals: Gain Muscle Mass, Loose Fat, Weight maintenance / Recomposition",
+            iconRes = R.drawable.ic_calories_restaurant_black_24dp,
+            onClickAction = { findNavController().navigate(R.id.navigation_calories) }
+        )
 
-        binding.btnCalories.setOnClickListener {
-            findNavController().navigate(R.id.navigation_calories)
-        }
+        addCard(
+            title = "Resting Heart Rate",
+            description = "Measure your normal resting heart rate based on your age, height, weight and give some recommendations",
+            iconRes = R.drawable.ic_heart_rate_black_24dp,
+            onClickAction = { findNavController().navigate(R.id.navigation_heart_rate) }
+        )
 
-        binding.btnHeartRate.setOnClickListener {
-            findNavController().navigate(R.id.navigation_heart_rate)
-        }
+        return binding.root
+    }
 
-        return root
+    private fun addCard(title: String, description: String, iconRes: Int, onClickAction: () -> Unit) {
+        val cardView = layoutInflater.inflate(R.layout.card_button, binding.cardsContainer, false)
+
+        val titleTextView = cardView.findViewById<TextView>(R.id.titleText)
+        val descriptionTextView = cardView.findViewById<TextView>(R.id.descriptionText)
+        val iconImageView = cardView.findViewById<ImageView>(R.id.iconImage)
+
+        titleTextView.text = title
+        descriptionTextView.text = description
+        iconImageView.setImageResource(iconRes)
+
+        cardView.setOnClickListener { onClickAction() }
+
+        binding.cardsContainer.addView(cardView)
     }
 
     override fun onDestroyView() {
