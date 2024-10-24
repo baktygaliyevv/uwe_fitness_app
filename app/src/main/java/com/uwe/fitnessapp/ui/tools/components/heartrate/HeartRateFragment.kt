63bbfe26d4d6b8e0c136973ac76f65212c96ecc7
baktygaliyevv <116110183+baktygaliyevv@users.noreply.github.com>
2024-10-24 15:32:52 +0100

    table in progress
    its not working though, still developing

--- app/src/main/java/com/uwe/fitnessapp/ui/tools/components/heartrate/HeartRateFragment.kt
+++ app/src/main/java/com/uwe/fitnessapp/ui/tools/components/heartrate/HeartRateFragment.kt
@@ -25,12 +25,29 @@ class HeartRateFragment : Fragment() {
         return binding.root
     }
 
+    data class HeartRateZone(
+        val targetZone: String,
+        val intensity: String,
+        val thrRange: String
+    )
+
     private fun calculateHeartRate() {
         val ageText = binding.ageInput.text.toString()
         if (ageText.isNotEmpty()) {
             val age = ageText.toInt()
 
             val maxHeartRate = 220 - age
+
+            val zones = listOf(
+                HeartRateZone("Maximum VO2 Max Zone", "90% - 100%", "${(maxHeartRate * 0.9).toInt()} - ${maxHeartRate}"),
+                HeartRateZone("Hard Anaerobic Zone", "80% - 90%", "${(maxHeartRate * 0.8).toInt()} - ${(maxHeartRate * 0.9).toInt()}"),
+                HeartRateZone("Moderate Aerobic Zone", "70% - 80%", "${(maxHeartRate * 0.7).toInt()} - ${(maxHeartRate * 0.8).toInt()}"),
+                HeartRateZone("Light Fat Burn Zone", "60% - 70%", "${(maxHeartRate * 0.6).toInt()} - ${(maxHeartRate * 0.7).toInt()}"),
+                HeartRateZone("Very Light Warm Up Zone", "50% - 60%", "${(maxHeartRate * 0.5).toInt()} - ${(maxHeartRate * 0.6).toInt()}")
+            )
+
+            binding.heartRateTable.layoutManager = LinearLayoutManager(requireContext())
+            binding.heartRateTable.adapter = HeartRateAdapter(zones)
 
         }
     }
