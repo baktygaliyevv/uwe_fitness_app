8bc83bbac2b9393d2e8360d34b2225122ff5377b
Denis Zhmurenko <d.zhmurenko@dodobrands.io>
2024-11-06 00:25:16 +0000

    open exercises group

--- app/src/main/java/com/uwe/fitnessapp/ui/exercises/ExercisesFragment.kt
+++ app/src/main/java/com/uwe/fitnessapp/ui/exercises/ExercisesFragment.kt
@@ -1,14 +1,19 @@
 package com.uwe.fitnessapp.ui.exercises
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.uwe.fitnessapp.R
 import com.uwe.fitnessapp.databinding.FragmentExercisesBinding
 import com.uwe.fitnessapp.models.ExercisesGroup
 import com.uwe.fitnessapp.utils.ReadJSON
@@ -16,7 +21,6 @@ import com.uwe.fitnessapp.utils.ReadJSON
 class ExercisesFragment : Fragment() {
 
     private var _binding: FragmentExercisesBinding? = null
-    private lateinit var listAdapter: ExercisesListAdapter
     private lateinit var exercisesData: ArrayList<ExercisesGroup?>
 
     // This property is only valid between onCreateView and
@@ -34,11 +38,31 @@ class ExercisesFragment : Fragment() {
         _binding = FragmentExercisesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        listAdapter = ExercisesListAdapter(requireContext(), exercisesData)
-        binding.listview.adapter = listAdapter
-        binding.listview.isClickable = true
+        for (group in exercisesData) {
+            val drawableResourceId: Int = resources.getIdentifier(group!!.image, "drawable", activity?.packageName)
+            addCard(group.type, drawableResourceId, {
+                val nav = findNavController()
+                nav.navigate(R.id.navigation_exercises_exercise, Bundle().apply {
+                    putString("label", group.type)
+                })
+            })
+        }
 
         return root
+    }
+
+    private fun addCard(title: String, iconRes: Int, onClickAction: () -> Unit) {
+        val cardView = layoutInflater.inflate(R.layout.fragment_exercise_item, binding.cardsContainer, false)
+
+        val titleTextView = cardView.findViewById<TextView>(R.id.exerciseItemTitle)
+        val iconImageView = cardView.findViewById<ImageView>(R.id.exerciseItemImage)
+
+        titleTextView.text = title
+        iconImageView.setImageResource(iconRes)
+
+        cardView.setOnClickListener { onClickAction() }
+
+        binding.cardsContainer.addView(cardView)
     }
 
     override fun onDestroyView() {
